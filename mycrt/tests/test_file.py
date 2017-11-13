16eace2eddd455398407828eb41e72c33df1d6b1
Jake Pickett <j.pickett67@yahoo.com>
2017-11-13 20:47:53 +0000

    Adding tests

---
+++ mycrt/tests/test_file.py
@@ -0,0 +1,17 @@
+#from server import mycrt
+import pytest
+
+"""
+if __name__ == '__main__':
+
+    if __package__ is None:
+        import sys
+        from os import path
+        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+        from server.mycrt import *
+    else:
+        from ..server.mycrt import *
+"""
+
+def test_simple(): 
+    assert 2 == 1
