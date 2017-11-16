494181634a451ede3e23e9413a6fe62335e8ccf0
AndrewCofano12 <andrewcofano@gmail.com>
2017-11-16 10:34:59 -0800

    finish replay v1

--- mycrt/server/replay.py
+++ mycrt/server/replay.py
@@ -2,30 +2,27 @@ import pymysql as sql
 import boto3
 import datetime
 
-hostname = "pi.cwsp4gygmyca.us-east-2.rds.amazonaws.com"
-username = "olive"
-password = "olivechinos"
-database = "CRDB"
-
-# replace with user input
-# hard coding is bad
-user_access_key_id = 'AKIAIE2W56K7DHZBQO4A'
-user_secret_access_key = 'F3TDRcrMu+75HR7OEJSfhtID6Z3tZppPPokQN5gY'
-
-transactionList = ['CREATE TABLE CUSTOMERS(
- ID INT NOT NULL,
- NAME VARCHAR (20) NOT NULL,
- AGE INT NOT NULL,
- ADDRESS CHAR (25),
- ORDERS VARCHAR(155))']
+transactionList = ['SELECT * FROM users', 'SELECT * FROM users', 'SELECT * FROM users', 'SELECT * FROM users']
 # def retrieveMetrics(connection):
 #    cur = connection.cursor()
 #    cur.execute("SELECT name, subsystem, status,  FROM INFORMATION_SCHEMA.INNODB_METRICS WHERE status = 'enabled' ORDER BY NAME")
 
 #    for line in cur.fetchall():
 #       print(line)
-cloudwatch = boto3.resource('cloudwatch')
-metric = cloudwatch.Metric('AWS/RDS', 'CPUUtilization')
+
+s3Client = boto3.client(
+   's3',
+   aws_access_key_id = user_access_key_id,
+   aws_secret_access_key = user_secret_access_key,
+   region_name = region)
+
+cloudwatch_client = boto3.client(
+   'cloudwatch',
+   aws_access_key_id = user_access_key_id,
+   aws_secret_access_key = user_secret_access_key,
+   region_name = region)
+
+#metric = cloudwatch_client.Metric('AWS/RDS', 'CPUUtilization')
 
 def replayTransactions(connection, transactions):
    cur = connection.cursor()
@@ -47,14 +44,23 @@ def replayTransactions(connection, transactions):
 
 myConnection = sql.connect(host = hostname, user = username, passwd = password, db = database)
 
-startTime = datetime.datetime.now()
+startTime = datetime.datetime.utcnow() - datetime.timedelta(minutes = 60)
 replayTransactions(myConnection, transactionList)
-endTime = datetime.datetime.now()
+endTime = datetime.datetime.utcnow()
 
-response = metric.get_statistics(
+response = cloudwatch_client.get_metric_statistics(
+    Namespace='AWS/RDS',
+    MetricName='FreeableMemory',
+    Dimensions = [{
+         "Name" : "DBInstanceIdentifier",
+         "Value" : "pi"
+    }],
     StartTime=startTime,
     EndTime=endTime,
-    Period=5,
+    Period=60,
+    Statistics=[
+        'Average'
+    ]
 )
 
 #retrieveMetrics(myConnection)
