9c8d47ec3e9b177e69dffbab3e01695f4de56859
Jake Pickett <j.pickett67@yahoo.com>
2017-11-27 14:09:31 -0800

    Starting REST API

--- mycrt/server/mycrt.py
+++ mycrt/server/mycrt.py
@@ -1,6 +1,6 @@
 # server.py
 from flask import Flask, render_template
-from .Analytics import *
+from Analytics import *
 
 application = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 
@@ -13,8 +13,30 @@ def index():
 # and send it to the user interface
 
 
-@application.route("/analytics")
+@application.route("/login", methods=["POST"])
+def login():
+	pubKey = flask.request.values.get('pubKey') # Your form's
+    privateKey = flask.request.values.get('privateKey') # input names
+    return check_login(pubKey, privateKey)
+
+@application.route("/capture/start", methods=["POST"])
+def capture():
+    db_name = flask.request.values.get('db') 
+    return start_capture()
+
+@application.route("/capture/end", methods=["POST"])
+def capture():
+    db_name = flask.request.values.get('db') 
+    return end_capture()
+
+@application.route("/replay", methods=["POST"])
+def replay():
+    db_name = flask.request.values.get('db') 
+    return start_replay()
+
+@application.route("/analytics", methods=["GET"])
 def analytics():
+	analyticsNumber = flask.request.args.get('id')
     return get_analytics()
 
 if __name__ == "__main__":
