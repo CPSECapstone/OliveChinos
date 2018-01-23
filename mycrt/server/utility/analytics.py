178fee93d2bf37bde3919c162e150f209a241b88
ajboyd2 <ajboyd@calpoly.edu>
2018-01-22 22:32:06 -0800

    Restructured general metric storage within S3. Changes reflected in code.

--- mycrt/server/utility/analytics.py
+++ mycrt/server/utility/analytics.py
@@ -2,15 +2,32 @@ import boto3
 import pickle
 import sys
 
+def get_capture_list(credentials):
+  s3_client = boto3.client('s3', **credentials)
+  key_list = [key['Key'] for key in s3_client.list_objects(Bucket="my-crt-test-bucket-olive-chinos")['Contents'] if key['Key'][-1] == "/"]
+  return key_list
+
+def get_replays_for_capture(credentials, capture_folder):
+  s3_client = boto3.client('s3', **credentials)
+  key_len = len(capture_folder)
+  key_list = [key['Key'] for key in s3_client.list_objects(Bucket="my-crt-test-bucket-olive-chinos")['Contents']]
+  general_capture_list = [key for key in key_list if key != capture_folder and key[:key_len] == capture_folder]
+  replay_list = [key for key in general_capture_list if ".replay" == key[-len(".replay"):]] 
+  return replay_list
+
 def get_analytics(credentials):
   analytics = "Pretend this is some analytics Data"
   region = credentials['region_name']
   s3_client = boto3.client('s3', **credentials)
-  metrics = retrieve_analytics(s3_client)
-
+  metrics = {}
+  capture_list = get_capture_list(credentials)
+  for capture in capture_list:
+    replay_list = get_replays_for_capture(credentials, capture)
+    metrics[capture] = {replay : retrieve_analytics(s3_client, log_key = replay) for replay in replay_list}
+  
   return metrics
 
-def retrieve_analytics(s3_client, bucket_id = "my-crt-test-bucket-olive-chinos", log_key = "test-metrics"):
+def retrieve_analytics(s3_client, bucket_id = "my-crt-test-bucket-olive-chinos", log_key = "test-folder/test-metrics"):
   bucket_obj = s3_client.get_object(
     Bucket = bucket_id,
     Key = log_key
