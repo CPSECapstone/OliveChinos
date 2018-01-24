a1cd8d1cd53636d4aadbcadf6b6c679a6db9217c
AndrewCofano12 <andrewcofano@gmail.com>
2018-01-24 13:35:35 -0800

    Redux state supports multiple captures/replays; in-progress tabs styled

--- mycrt/static/components/Replay.jsx
+++ mycrt/static/components/Replay.jsx
@@ -1,51 +1,68 @@
-import React, { Component } from 'react';
-import jquery from 'jquery';
-import { Button } from 'react-bootstrap';
-import {startReplay} from '../actions';
-import {connect} from 'react-redux';
-import {setReplay} from '../actions';
+import React, { Component } from 'react'
+import jquery from 'jquery'
+import { Button } from 'react-bootstrap'
+import { startReplay } from '../actions'
+import { connect } from 'react-redux'
+import { setReplay, startNewReplay, stopReplay } from '../actions'
 
 /* Use this element as a reference when creating components*/
 
 class Replay extends React.Component {
-
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       replay: this.props.replay,
       replayActive: this.props.replayActive
-    };
+    }
 
     //binding required for callback
-    this.startReplay = this.startReplay.bind(this);
+    this.startReplay = this.startReplay.bind(this)
+    this.addReplay = this.addReplay.bind(this)
   }
 
   startReplay() {
-    this.setState({replay: 'Replay Active'});
-    this.props.dispatch(setReplay());
-    jquery.post(window.location.href + 'replay', (data) => {
-      this.setState({replay: "Replay Inactive"});
-      this.props.dispatch(setReplay());
-      console.log(data);
-    });
+    this.setState({ replay: 'Replay Active' })
+    this.props.dispatch(setReplay())
+    jquery.post(window.location.href + 'replay', data => {
+      this.setState({ replay: 'Replay Inactive' })
+      this.props.dispatch(setReplay())
+      console.log(data)
+    })
   }
 
-  render () {
+  addReplay() {
+    this.setState({ replay: 'Replay Active' })
+    this.props.dispatch(startNewReplay())
+    jquery.post(window.location.href + 'replay', data => {
+      this.setState({ replay: 'Replay Inactive' })
+      this.props.dispatch(stopReplay())
+      console.log(data)
+    })
+  }
+
+  render() {
     return (
       <div>
-        <hr/>
-        <Button style={{marginLeft:'20px'}} bsSize="large" bsStyle="success" onClick={this.startReplay}>
+        <hr />
+        <Button
+          style={{ marginLeft: '20px' }}
+          bsSize="large"
+          bsStyle="success"
+          onClick={this.addReplay}
+        >
           Start Replay
         </Button>
-        <hr/>
-        <h4 style={{marginLeft:'20px'}}>{this.state.replay}</h4>
+        <hr />
+        <h4 style={{ marginLeft: '20px' }}>{this.state.replay}</h4>
       </div>
-    );
+    )
   }
 }
 
-
-const mapStateToProps = state => ({replayActive: state.replayActive, replay: state.replay})
+const mapStateToProps = state => ({
+  replayActive: state.replayActive,
+  replay: state.replay
+})
 
 export default connect(mapStateToProps)(Replay)
