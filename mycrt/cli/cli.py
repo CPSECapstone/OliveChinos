349829df354db1a2332b440afab2f43bf2c4dc10
Courtney Brown <cbrown83@calpoly.edu>
2018-01-24 21:15:04 -0800

    cli analyze pulls metrics from server and averages datapoints

--- mycrt/cli/cli.py
+++ mycrt/cli/cli.py
@@ -1,25 +1,55 @@
 import click
+import requests #rest api
 
 default_capture_name='capture_1'
 default_replay_name='replay_1'
 
 @click.group()
 def cli(): 
+    """Tool to analyze database workloads"""
     pass
 
+
 @cli.command()
+@click.argument('db-instance')
 @click.option('--capture-name', default=default_capture_name,
         help='nickname for capture')
-@click.argument('db-instance')
 @click.option('-i', '--interactive', is_flag=True)
 @click.option('-s', '--schedule', prompt='Please enter <start_time,end_time>')
 @click.option('--view-captures', is_flag=True)
-def capture(capture_name, db_instance, i_flag, s_flag, view): 
+def capture(db_instance, capture_name, i_flag, s_flag, view): 
+    """-capture a database workload"""
     click.echo('capturing')
 
+
 @cli.command()
-def replay(): 
+@click.argument('db-instance')
+@click.option('--replay-name', default=default_replay_name,
+        help='nickname for replay')
+@click.option('--capture-name', 
+        help='name of capture to replay')
+@click.option('-f', '--fast-mode', is_flag=True,
+        help='skip over time periods with low activity while replaying')
+@click.option('-r', '--raw-mode', is_flag=True,
+        help='play capture as recorded')
+@click.option('--restore', is_flag=True,
+        help='restore initial database state upon replay completion')
+#default = database_name + date/time started 
+def replay(db_instance, replay_name, capture_name): 
+    """-replay a database workload"""
     click.echo('replaying')
+
+
+# Compute the average of all the average metric data points 
+def get_average(metric_list): 
+    average = 0
+    for metric in metric_list: 
+        data_point = metric['Average']
+        average += data_point
+
+    return average / len(metric_list)
+
+METRIC_NAMES = ['CPUUtilization', 'FreeableMemory', 'ReadIOPS', 'WriteIOPS']
 
 @cli.command() 
 @click.option('--replay-name', multiple=True, 
@@ -32,4 +62,26 @@ def replay():
 @click.option('--output-file', 
         help='file to export graph to')
 def analyze(replay_name, include_metric, time_frame, output_file):
+    """-analyze database performance from historic replay"""
+
     click.echo('analyzing')
+    analytics = requests.get('http://localhost:5000/analytics')
+    if analytics.status_code != 200:
+        #error
+        raise ApiError('GET /analytics/ {}'.format(analytics.status_code))
+    #print(analytics.json())
+    json_input = analytics.json()
+    try:
+        for metric_name in json_input: 
+            if metric_name in METRIC_NAMES: 
+                metric_list = json_input[metric_name]
+                average_metric = get_average(metric_list)
+                formatted_output = metric_name + ': ' + str(average_metric)
+                click.echo(formatted_output)
+            if metric_name not in METRIC_NAMES: 
+                click.echo(metric_name + ': ' + str(json_input[metric_name]))
+
+    except (ValueError, KeyError, TypeError): 
+        print('JSON format error')
+         
+
