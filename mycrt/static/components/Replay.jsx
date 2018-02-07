380ef9433cc5022a82f651a5e61fa5a0a936ba65
cpirvu <costin_64@yahoo.com>
2018-02-07 13:12:14 -0800

    additions to create replay form

--- mycrt/static/components/Replay.jsx
+++ mycrt/static/components/Replay.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 import jquery from 'jquery'
-import { Button } from 'react-bootstrap'
+import {
+  Button,
+  Form,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  HelpBlock,
+  DropdownButton,
+  MenuItem
+} from 'react-bootstrap'
 import { startReplay } from '../actions'
 import { connect } from 'react-redux'
 import { setReplay, startNewReplay, stopReplay } from '../actions'
@@ -63,26 +72,56 @@ class Replay extends React.Component {
     return <ul>{currentReplays}</ul>
   }
 
+  displayDBInstancesInDropdown() {
+    let currentDBInstances = []
+    jquery.get(wondow.location.href + 'db_instances', data => {})
+    for (var i = 0; i < data.DBInstances; i++) {
+      currentDBInstances.push(<MenuItem>eventKey={i}</MenuItem>)
+    }
+  }
+
   render() {
     return (
       <div>
         <hr />
         <form>
-           <FormGroup
-             controlId="formBasicText"
-             validationState={this.getValidationState()}
-           >
-             <ControlLabel>Working example with validation</ControlLabel>
-             <FormControl
-               type="text"
-               value={this.state.value}
-               placeholder="Replay Name"
-               onChange={this.handleReplayNameChange}
-             />
-             <FormControl.Feedback />
-             <HelpBlock>Validation is based on string length.</HelpBlock>
-           </FormGroup>
-      </form>
+          <FormGroup
+            controlId="formBasicText"
+            //validationState={this.getValidationState()}
+          >
+            <ControlLabel>Working example with validation</ControlLabel>
+            <FormControl
+              type="text"
+              value={this.state.value}
+              placeholder="Replay Name"
+              onChange={this.handleReplayNameChange}
+            />
+            <FormControl.Feedback />
+            <HelpBlock>Validation is based on string length.</HelpBlock>
+          </FormGroup>
+        </form>
+
+        <DropdownButton
+          //bsStyle={title.toLowerCase()}
+          title="Database Instance"
+          //key={i}
+          //id={`dropdown-basic-${i}`}
+        >
+          <MenuItem eventKey="1">DB Instance 1</MenuItem>
+          <MenuItem eventKey="2">DB Instance 2</MenuItem>
+          <MenuItem eventKey="3">DB Instance 3</MenuItem>
+        </DropdownButton>
+
+        <DropdownButton
+          //bsStyle={title.toLowerCase()}
+          title="Capture To Replay On"
+          //key={i}
+          //id={`dropdown-basic-${i}`}
+        >
+          <MenuItem eventKey="1">Capture 1</MenuItem>
+          <MenuItem eventKey="2">Capture 2</MenuItem>
+          <MenuItem eventKey="3">Capture 3</MenuItem>
+        </DropdownButton>
 
         <Button
           style={{ marginLeft: '20px' }}
