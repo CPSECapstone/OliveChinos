0aa16c5a147724d0d274e8d9014a31c4cb0b2865
Courtney Brown <cbrown83@calpoly.edu>
2018-03-05 12:42:32 -0800

    scheduled captures fully functional

--- mycrt/server/utility/scheduler.py
+++ mycrt/server/utility/scheduler.py
@@ -10,22 +10,30 @@ Scheduled captures also have a scheduler to run them at their specified start ti
 
 capture_processes holds all of the capture processes
 Key: capture_name
-Value: (scheduler object, 
-        start_process_id, 
-        end_process_id,
-        start_process_schedule_id, 
-        end_process_schedule_id)
+Value: (start_process_id, 
+        end_process_id)
 """
 capture_processes = {}
 
+
+"""
+capture_scheduler holds the scheduler and start/end id's for each capture
+note if the capture is interactive, both scheduler object and 
+end_capture_schedule_id will be None
+
+Key: capture_name
+Value: (scheduler object, 
+        start_capture_schedule_id, 
+        end_capture_schedule_id)
+"""
+capture_scheduler = {}
 
 # this handles initiating the capture, creating the process for a schedulerd
 # assumes if no end time specified, capture is interactive 
 def new_capture_process(credentials, capture_name, db_name, start_time, end_time): 
 
-    print('BLAHHHHHHHH', file=sys.stderr)
-    capture_scheduler = None
     start_capture_schedule_id = None
+    start_capture_process = None
     end_capture_schedule_id = None
     end_capture_process = None
 
@@ -36,50 +44,65 @@ def new_capture_process(credentials, capture_name, db_name, start_time, end_time
         start_capture_process.start()
         start_capture_process.join()
 
+        capture_processes[capture_name] = (start_capture_process, None)
+
+
         #TODO remove this once tested
         if start_capture_process.is_alive():
             print('new interactive capture started: ' + capture_name, file=sys.stderr)
 
     else: #scheduled capture
-        print('AAAAAAAAAAA', file=sys.stderr)
-        capture_scheduler = sched.scheduler(time.time, time.sleep)
+        print('scheduled capture', file=sys.stderr)
+        scheduler = sched.scheduler(time.time, time.sleep)
 
-        start_capture_sched_id= capture_scheduler.enterabs(start_time_in_seconds, 
+        start_time_in_seconds = _get_epoch_time(start_time)
+        print(start_time_in_seconds, file=sys.stderr)
+        start_priority = 1
+        start_capture_sched_id = scheduler.enterabs(start_time_in_seconds, 
                                     start_priority, 
-                                    run_start_capture_process, 
+                                    _run_start_capture, 
                                     (capture_name, db_name, start_time))
 
-        end_capture_sched_id = capture_scheduler.enterabs(end_time_in_seconds, 
+        end_time_in_seconds = _get_epoch_time(end_time)
+        end_priority = 1
+        end_capture_sched_id = scheduler.enterabs(end_time_in_seconds, 
                                     end_priority, 
-                                    run_end_capture_process, 
-                                    (credentials, capture_name, db_name, end_time))
+                                    _run_end_capture,
+                                    (credentials, capture_name, db_name))
 
         #TODO test to make sure no delay from start time to actual run time
-        schedule_process = multiprocessing.Process(target=capture_scheduler.run) 
+        schedule_process = multiprocessing.Process(target=scheduler.run) 
         schedule_process.start()
         schedule_process.join()
 
-                #schedule end_capture event
-        print('BBBBBBBB', file=sys.stderr)
-        end_capture_process = multiprocessing.Process(target=end_capture, 
-                args=(credentials, capture_name, db_name))
+        #add to dictionary in case user wants to cancel scheduled event
+        capture_scheduler[capture_name] = (scheduler, 
+                                            start_capture_sched_id,
+                                            end_capture_sched_id)
 
-        end_time_in_seconds= get_delta(end_time)
-        end_priority = 1
+def _run_start_capture(capture_name, db_name, start_time): 
+    # a capture must start before it can end so safe to set end_capture_id=None
+    start_process_id = _run_process(start_capture, (capture_name, db_name, start_time))
+    capture_processes[capture_name] = (start_process_id, None)
 
-        end_capture_schedule_id = capture_scheduler.enterabs(end_time_in_seconds,
-                end_priority, end_capture_process.start)
+def _run_end_capture(credentials, capture_name, db_name): 
+    # don't want to overwrite start_capture_id
+    start_process_id = capture_processes[capture_name][0]
+    end_process_id = _run_process(end_capture, (credentials, 
+                                                capture_name, 
+                                                db_name))
 
-        #run the task at the given times
-    capture_processes[capture_name] = (capture_scheduler,
-                                        start_capture_process, 
-                                        end_capture_process,
-                                        start_capture_schedule_id,
-                                        end_capture_schedule_id)
-    
-    print('FOOOOOOOO', file=sys.stderr)
+    capture_processes[capture_name] = (start_process_id, 
+                                        end_process_id)
 
+def _run_process(function_to_run, args): 
+    print('running process', file=sys.stderr)
+    process = multiprocessing.Process(target=function_to_run, args=args)
+    process.start()
+    process.join()
+    return process
 
+"""---------- CANCEL CAPTURE -------------"""    
 #currently unused, useful later on
 def cancel_capture_process(capture_name): 
     #TODO check if the capture was scheduled
@@ -115,7 +138,8 @@ def cancel_capture_process(capture_name):
 
     #delete dictionary entry
     del capture_processes[capture_name]
-    
+
+
 #convert datetime string to seconds since epoch for use by scheduler
 #example input: '2018-03-01 00:09'
 #example output: '1519891740.0'
@@ -124,11 +148,4 @@ def _get_epoch_time(raw_time):
     eight_hours = timedelta(hours=8).total_seconds()
     return time.mktime(dt_obj.timetuple()) - eight_hours
 
-def _run_process(function_to_run, args): 
-    run_time = _get_epoch_time(time)
-    priority = 1
-
-    process = multiprocessing.Process(target=function_to_run, args=args)
-    process.start()
-    process.join()
 
