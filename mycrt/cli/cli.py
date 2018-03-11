fbf97282aa855307e53a734f8780d5134daa5b43
Courtney Brown <cbrown83@calpoly.edu>
2018-03-10 21:53:26 -0800

    cli equipped for functionality supported in front-end; waiting on a couple endpoints and other back-end fixes

--- mycrt/cli/cli.py
+++ mycrt/cli/cli.py
@@ -10,85 +10,142 @@ def cli():
     pass
 
 
-'''-------------VIEW-------------'''
-def _view_captures(db_instance): 
-    captures = requests.get('http://localhost:5000/capture/list')
-
-    if captures.status_code != 200: #there was an error
-        raise requests.HTTPError('GET /analytics/ {}'.format(captures.status_code))
-
-    json_input = captures.json()
-
-    click.echo(json_input)
-
-@cli.command()
-def view():
-    '''View previous capture and replay information'''
+'''-------------CAPTURE-------------'''
+@cli.group()
+def capture(): 
+    '''-capture a database workload'''
     pass
 
+@capture.command(short_help='-view capture information')
+@click.option('-c', '--completed', is_flag=True, 
+        help='-show captures that have completed')
+@click.option('-o', '--ongoing', is_flag=True, 
+        help='-show captures occuring right now')
+@click.option('-s', '--scheduled', is_flag=True, 
+        help='-show captures scheduled for an upcoming time')
+def view(completed, ongoing, scheduled):
+    '''-view capture information
+    If no options are given, all captures will be displayed organized by status
+    '''
+    if completed: 
+        click.echo('---Completed Captures---\n'+_get_capture_list('completed'))
+    if ongoing: 
+        click.echo('---Ongoing Captures---\n'+_get_capture_list('ongoing'))
+    if scheduled: 
+        click.echo('---Scheduled Captures---\n'+_get_capture_list('scheduled'))
 
-'''-------------CAPTURE-------------'''
-def _start_capture(db_instance, capture_name): 
+    if not completed and not ongoing and not scheduled: 
+        click.echo('---Completed Captures---\n'+_get_capture_list('completed'))
+        click.echo('---Ongoing Captures---\n'+_get_capture_list('ongoing'))
+        click.echo('---Scheduled Captures---\n'+_get_capture_list('scheduled'))
 
-    date_time=datetime.utcnow().strftime('%b/%d/%Y_%H:%M:%S')
-    start_time=date_time.split('_')[1]
+def _get_capture_list(status): 
+    endpoint='list_' + status
+    captures = requests.get('http://localhost:5000/capture/'+endpoint)
+
+    if captures.status_code != 200: #there was an error
+        raise requests.HTTPError('GET /captures/ {}'.format(captures.status_code))
+
+    return str(captures.json())
+
+@capture.command(short_help='-start capturing a database workload')
+@click.argument('db-instance')
+@click.option('-n', '--capture-name', 
+        help='-a unique name for the capture')
+@click.option('-s', '--start-time',
+        help='-time to start a scheduled capture, format: YYYY/MM/DD_HH:MM:SS')
+@click.option('-e', '--end-time',
+        help='-time to end a scheduled capture, format: YYYY/MM/DD_HH:MM:SS')
+def start(db_instance, capture_name, start_time, end_time): 
+    '''-start capturing a database workload 
+        
+        To start an interactive capture, specify an Amazon RDS instance identifier
+        and a name (optional). End this capture using the "end" command.
+
+        To schedule a capture in the future, include the -s and -e flags to 
+        set the start and end times of the capture. Note you must include both 
+        a start and end time if you desire to schedule a capture.
+    '''
+
+    if not start_time: #interactive capture
+        date_time=datetime.utcnow().strftime('%b/%d/%Y_%H:%M:%S')
+        start_time=date_time.split('_')[1]
 
     task = {'db': db_instance, 
+            'captureName': capture_name,
             'startTime': start_time
     }
 
-    if capture_name: #use default
-        #TODO verify that this name is unique. return 403 if not? 
-        task['captureName'] = capture_name
-        
     resp = requests.post('http://localhost:5000/capture/start', json=task)
 
     if resp.status_code != 200:
         raise requests.HTTPError('POST /tasks/ {}'.format(resp.status_code))
 
 
-def _stop_capture(db_instance, capture_name): 
+@capture.command(short_help='-end an ongoing capture')
+@click.argument('capture-name')
+def end(capture_name): 
+    '''-end an ongoing capture
 
-    if not capture_name: 
-        #TODO throw error
-        pass
-
-    task = {'db': db_instance, 
-            'captureName': capture_name, 
-    }
+    Note the specified capture must currently be in progress in order to end it. 
+    '''
+    #TODO we might need to add the db_name in here
+    task = {'captureName': capture_name}
 
     resp = requests.post('http://localhost:5000/capture/end', json=task)
 
     if resp.status_code != 200: 
         raise requests.HTTPError('POST /tasks/ {}'.format(resp.status_code))
 
+@capture.command(short_help='-cancel a capture')
+@click.argument('capture-name')
+def cancel(capture_name): 
+    """-cancel a capture 
 
-@cli.command()
-@click.argument('db-instance')
-@click.option('--capture-name', 
-        help='nickname for capture')
-@click.option('-i', '--interactive', is_flag=True,
-        help='start capture in interactive mode, stop capture with "--stop" command')
-@click.option('-s', '--schedule', nargs=2, 
-        help='schedule capture to start at specified time; input: start_time stop_time')
-@click.option('--stop', is_flag=True,
-        help='stop specified capture in interactive mode; input: capture_name')
-def capture(db_instance, capture_name, interactive, schedule, stop): 
-    '''-capture a workload'''
-    #todo: validate capture name and db instance
-    if stop: 
-        _stop_capture(db_instance, capture_name)
-        click.echo('stopped capture ')
+    Cancelling a capture will prevent the system from processing the 
+    capture details. No record of the capture will be stored. 
 
-    else: 
-        _start_capture(db_instance, capture_name)
-        click.echo('started catpure ')
+    This is especially useful in the case that an interactive capture is started 
+    and forgotten about (accumulating many unintended hours/days of data). It can 
+    also be used to cancel an upcoming scheduled capture or a scheduled capture 
+    that is currently in progress.
+    """
+    #TODO hook up the endpoint
+    pass
 
+@capture.command(short_help='-delete a completed capture')
+@click.argument('capture-name')
+def delete(capture_name):
+    '''-delete a completed capture 
+    
+    This will remove any record of the capture. 
+    '''
+    #NOTE did we decide that deleting a capture will also remove all replays associated with it? 
+    task={'capture': capture_name}
+
+    resp = requests.delete('http://localhost:5000/capture/delete', json=task)
+
+    if resp.status_code != 200: 
+        raise requests.HTTPError('DELETE /capture/delete/ {}'.format(resp.status_code))
 
 
 '''-------------REPLAY-------------'''
-def _start_replay(db_instance, capture_name, replay_name, fast_mode, restore): 
+@cli.group()
+def replay(): 
+    '''-replay a database workload'''
+    pass
 
+@replay.command()
+@click.argument('capture-name')
+@click.argument('db-instance')
+@click.option('-n', '--name', 
+        help='-name for the replay; default name will be given if not specified')
+@click.option('-f', '--fast-mode', is_flag=True,
+        help='-skip over time periods with low activity while replaying')
+@click.option('-r', '--restore', is_flag=True,
+        help='-restore initial database state upon replay completion')
+def start(replay_name, db_instance, capture_name, fast_mode, restore): 
+    '''-start a new replay immediately'''
     date_time=datetime.utcnow().strftime('%b/%d/%Y_%H:%M:%S')
     start_time=date_time.split('_')[1]
 
@@ -96,7 +153,8 @@ def _start_replay(db_instance, capture_name, replay_name, fast_mode, restore):
             'captureName': capture_name,
             'fastMode': fast_mode,
             'restoreDb': restore,
-            'startTime': start_time
+            'startTime': start_time,
+            'replayName':''
     }
 
     if replay_name: 
@@ -107,48 +165,51 @@ def _start_replay(db_instance, capture_name, replay_name, fast_mode, restore):
     if resp.status_code != 200:
         raise requests.HTTPError('POST /tasks/ {}'.format(resp.status_code))
 
-@cli.command()
-@click.argument('db-instance')
+@replay.command(short_help='-delete a completed replay')
 @click.argument('capture-name')
-@click.option('--replay-name', 
-        help='nickname for replay')
-@click.option('-f', '--fast-mode', is_flag=True,
-        help='skip over time periods with low activity while replaying')
-@click.option('--restore', is_flag=True,
-        help='restore initial database state upon replay completion')
-#default = database_name + date/time started 
-def replay(db_instance, capture_name, replay_name, fast_mode, restore): 
-    '''-replay a database workload'''
+@click.argument('replay-name')
+def delete(capture_name, replay_name): 
+    '''-delete a completed replay 
+    
+    Deleting a replay will delete all data affiliated with it
+    '''
+    task={'captureName': capture_name, 
+            'replayName': replay_name
+    }
+    resp = requests.delete('http://localhost:5000/replay/delete', json=task)
 
-    _start_replay(db_instance, capture_name, replay_name, fast_mode, restore)
-    click.echo('replaying raw mode')
+    if resp.status_code != 200:
+        raise requests.HTTPError('POST /tasks/ {}'.format(resp.status_code))
 
+
+@replay.command()
+def view(): 
+    '''-view ongoing replays'''
+    replays = requests.get('http://localhost:5000/replay/list')
+
+    if replays.status_code != 200: #there was an error
+        raise requests.HTTPError('GET /replay/list {}'.format(captures.status_code))
 
 '''-------------ANALYZE-------------'''
-# Compute the average of all the average metric data points 
-def get_average(metric_list): 
-    average = 0
-    for metric in metric_list: 
-        data_point = metric['Average']
-        average += data_point
+@cli.group()
+def analyze(): 
+    '''-view database performance from a replay'''
+    pass
 
-    return average / len(metric_list)
+@analyze.command(name='list-metrics')
+def list_metrics(): 
+    '''-list the metrics available to analyze'''
+    metric_options = 'CPUUtilization\nFreeableMemory\nReadIOPS\nWriteIOPS'
+    click.echo('Available Metrics:\n' + metric_options)
 
-
-@cli.command() 
-@click.option('--replay-name', multiple=True, 
-        help='replay nickname to analyze')
-@click.option('--include-metric', multiple=True, 
-        help='metric to calculate')
-@click.option('--time-frame', type=(int, int), 
-        default=(0,0),
-        help='specify time frame to analyze')
-@click.option('--output-file', 
-        help='file to export graph to')
-@click.option('--raw', '-r', is_flag = True,
-        help='get raw json format')
-def analyze(replay_name, include_metric, time_frame, output_file, raw):
-    '''-analyze database performance from historic replay'''
+@analyze.command()
+@click.argument('replay-name', nargs=-1)
+@click.option('-m', '--metric-name', multiple=True, 
+        help='-the name of the metric; use command "list-metrics to see supported options"')
+@click.option('-r', '--raw', is_flag = True,
+        help='-get raw json format')
+def view(replay_name, metric_name, raw):
+    '''-view metrics for any number of replays'''
 
     analytics = requests.get('http://localhost:5000/analytics')
     if analytics.status_code != 200: #there was an error
@@ -184,3 +245,12 @@ def analyze(replay_name, include_metric, time_frame, output_file, raw):
 
         except (ValueError, KeyError, TypeError): 
             traceback.print_exc() 
+
+# Compute the average of all the average metric data points 
+def get_average(metric_list): 
+    average = 0
+    for metric in metric_list: 
+        data_point = metric['Average']
+        average += data_point
+
+    return average / len(metric_list)
