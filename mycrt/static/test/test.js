d79267d13fd8fcd638861e439fd605f4a4a2c84f
YENG <yengkerngtan@gmail.com>
2018-04-18 16:54:31 -0700

    Added Mocha and updated README

---
+++ mycrt/static/test/test.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+//var expert = require('chai');
+//var shallow = require('enyzme');
+//var sinon = require('sinon');
+
+//var graphComponent = require('../components/Graph');
+//import { expect } from 'chai';
+//import { shallow } from 'enzyme';
+//import sinon from 'sinon';
+
+//import graphComponent from '../components/Graph';
+
+describe('Array', function() {
+  describe('#helloworld()', function() {
+    it('this is a test to show that we have some form of front-end testing environment', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+    /*
+    it('renders three <Foo /> components', () => {
+      const wrapper = shallow(<graphComponent />);
+      expect(wrapper.find(Foo)).to.have.length(3);
+    });
+    */
+  });
+});
